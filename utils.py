f5ab50bbfa22b96a0b481803cf5c5a1bc4dff504
Lili Yu <liliyu@Lilis-MacBook-Air.local>
2025-12-30 10:59:10 -0800

    remove openai client

--- utils.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-from config import OPENAI_API_KEY
-
-openai_client = None
-def get_openai_client():
-    global openai_client
-    if openai_client is None:
-        openai_client = OpenAI(api_key=OPENAI_API_KEY)
-    return openai_client
